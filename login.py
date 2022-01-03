e347697960a88daa630577f6272fe9697658b638
WBs234 <96954406+WBs234@users.noreply.github.com>
2022-01-03 18:23:44 -0300

    Update login.py

--- login.py
+++ login.py
@@ -5,7 +5,6 @@ os.system("clear")
 
 class bcolors:
     s = '\033[92m' #GREEN
-    Cadastrastrado com sucesso = '\033[93m' #YELLOW
     n = '\033[91m' #RED
 print(' _  _  _ ______       _______ _______  ______ _____  _____  _______')
 time.sleep(0.3)
@@ -27,7 +26,7 @@ input("certo? digite [s ou n]: ")
 time.sleep(1.0)
 
 print("\n\n")
-print("Cadastrado com sucesso\n")
+print(033[92m"Cadastrado com sucesso\n")
 os.system ("clear")
 print(f"Olá, bem vindo {login} ao meu teste de divisão")
 print(" ")
