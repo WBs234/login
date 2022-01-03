80a30ea6aacdd5bb70fba526bf5c9cdf1edf8552
WBs234 <96954406+WBs234@users.noreply.github.com>
2022-01-03 12:30:22 -0300

    Update login.py

--- login.py
+++ login.py
@@ -8,7 +8,6 @@ time.sleep(0.3)
 print(" |__|__| |_____]      ______| |_____  |    \_ __|__ |          |  ")
 time.sleep(1.0)
 login = imput("Login:")
-time.sleep(1.0)
 print("Seu nome é" , login , ",certo?"
 time.sleep(1.0)
 print("Cadastrado com sucesso")
