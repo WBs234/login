87c51be60ef023c5f2c34e63b46d942405467843
WBs234 <96954406+WBs234@users.noreply.github.com>
2022-01-03 18:34:41 -0300

    Update login.py

--- login.py
+++ login.py
@@ -3,16 +3,13 @@ import time
 
 os.system("clear")
 
-class bcolors:
-    s = '\033[92m' #GREEN
-    n = '\033[91m' #RED
-print(' _  _  _ ______       _______ _______  ______ _____  _____  _______')
+print('\033[92m _  _  _ ______       _______ _______  ______ _____  _____  _______')
 time.sleep(0.3)
 
 print(' |  |  | |_____]      |______ |       |_____/   |   |_____]    |   ')
 time.sleep(0.3)
 
-print(' |__|__| |_____]      ______| |_____  |    \_ __|__ |          |  \n')
+print(' |__|__| |_____]      ______| |_____  |    \_ __|__ |          |  \n\033[93m')
 time.sleep(1.0)
 
 login = input("Login:")
@@ -22,13 +19,13 @@ time.sleep(1.0)
 
 print("Seu nome é: " + login)
 
-input("certo? digite [s ou n]: ")
+input("certo? digite " , "\033[92m[s ou n]: ")
 time.sleep(1.0)
 
 print("\n\n")
 print("Cadastrado com sucesso\n")
 os.system ("clear")
-print(f"Olá, bem vindo {login} ao meu teste de divisão")
+print(f"\033[93mOlá, bem vindo {login} ao meu teste de divisão")
 print(" ")
 num1=float(input(" digite o primeiro numero:"))
 num2=float(input("digite o segundo numero:"))
