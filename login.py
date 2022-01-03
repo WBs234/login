95b67320e6fbeda59eede0e9f2abaa99cd56143c
WBs234 <96954406+WBs234@users.noreply.github.com>
2022-01-03 13:04:32 -0300

    Update login.py

--- login.py
+++ login.py
@@ -9,6 +9,6 @@ print(" |__|__| |_____]      ______| |_____  |    \_ __|__ |          |  ")
 time.sleep(1.0)
 login = input("Login:")
 import time
-print("Seu nome é" , login")
+print("Seu nome é" , login)
 input("certo? digite sim ou não")
 print("Cadastrado com sucesso")
